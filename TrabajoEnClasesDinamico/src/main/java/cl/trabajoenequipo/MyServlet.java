15398564fb73bf0bd7bcab6da3916968220d8942
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2022-04-25 23:06:27 -0400

    Add files via upload

---
+++ TrabajoEnClasesDinamico/src/main/java/cl/trabajoenequipo/MyServlet.java
@@ -0,0 +1,51 @@
+package cl.trabajoenequipo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+        // response.getWriter().append("Server at: ").append(request.getContextPath());
+    	
+    	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession(true);
+		
+		String uname = (String)session.getAttribute("username");
+		out.println("username is " + uname);
+		System.out.println(uname);
+
+	}
+
+}
